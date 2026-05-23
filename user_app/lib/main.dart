f3583b8481398042c15b99b019c787151026c129
Amelia Rizki Kusuma Ningrum <amelia_rkn@teknokrat.ac.id>
2026-05-23 19:57:03 +0700

    feat: add bottom navigation

--- user_app/lib/main.dart
+++ user_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home/home_page.dart';
+import 'widgets/bottom_navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: const BottomNavbar(),
     );
   }
 }
